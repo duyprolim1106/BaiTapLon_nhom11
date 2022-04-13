1c5c53be23a4b02df3dc23c7f01217743df16526
HungHoang1 <2051063705@e.tlu.edu.vn>
2022-04-13 10:21:20 +0700

    chinh sua js phan login dang nhap

--- assets/js/login.js
+++ assets/js/login.js
@@ -30,19 +30,12 @@ function login(){
     event.preventDefault();
     var username = document.getElementById('txtEmail').value;
     var password = document.getElementById('txtPW').value
-
     var val = check_user(username, password, users);
     if(val != false){
-        var date = new Date();
-        exp_date = date.getTime() + 10 * 24 * 60 * 60 * 1000;
-        date.setTime(exp_date);
-        document.cookie = "username=" + val.username + ";expires=" + date;
-        document.cookie = "permission=" + val.permission + ";expires=" + date;
         if (val.permission == "admin") {
             location.href = '../../admin/admin.html';
         } else if (val.permission == "user") {
-            // location.href = "../../admin/user.html";
-            alert("Đăng nhập với người dùng")
+            location.href = "../../admin/user.html";
         } else{
             alert("username must contant '@' and '_'");
         }
